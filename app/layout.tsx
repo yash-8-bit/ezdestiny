8b802afcd2422f2e8eef63a756b59e8b3a5dde34
Yash <yash008.108@gmail.com>
2026-06-22 08:28:37 +0530

    Project Intialized

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+import "./globals.css";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`h-full antialiased`}
+    >
+      <body className="min-h-full">{children}</body>
+    </html>
+  );
+}
